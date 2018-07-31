034755c6de6094ee8376a2a79160f5e5c25963c0
Prateek Anand <bizprat@gmail.com>
2018-07-31 22:01:00 +0530

    Added maintenance mode

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
 	var now = new Date().toString();
 	console.log(`${now}: ${req.method} ${req.originalUrl}`);
 	next();
 });
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('currentYear', () => {
 	return new Date().getFullYear();
